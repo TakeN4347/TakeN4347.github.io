e6abd841b789c20f38b3947524c9dcc6b14d0430
Steven Myte Antonio <Stevenmyte43@gmail.com>
2021-03-11 10:46:09 +0800

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,5 @@
-// Add your custom JavaScript in this file
-
 function scrollFunction() {
-  /**
-   *Check what position on the page the scrollbar is, so that no-shadow class is only applied while
-   * the navbar is above the header container, but navbar has a bottom shadow after that
-   */
+
   if ($(document).scrollTop() < 548) {
     $('.navbar-fixed nav').addClass('no-shadow');
   } else {
@@ -12,17 +7,11 @@ function scrollFunction() {
   }
 }
 
-// Everything inside this function will fire once the DOM is ready for JS
 $(function() {
 
-  // Activate Materialize's sidenav animation with jQuery
   $('.sidenav').sidenav();
 
-  // As the user scrolls, call scrollFunction
   window.onscroll = function() {
     scrollFunction();
   };
-
-  // ====== Start custom JS below here! ======
-
-}); // end of document ready
+});
